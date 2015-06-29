80d5908f237c064203ae4cb4e920149bc2fe577d
Bert De Knijf <bert.deknijf@gmail.com>
2015-06-29 21:05:24 +0200

    create droplet test1

--- droplet.py
+++ droplet.py
@@ -1,6 +1,7 @@
 __author__ = 'bdknijf'
 import json
-import urllib2
+#import urllib2
+import requests
 
 new_droplet = {
   "name": "test_api",
@@ -8,14 +9,23 @@ new_droplet = {
   "size": "512mb",
   "image": "ubuntu-14-04-x64",
   "ssh_keys": "bert",
-  "backups": false,
-  "ipv6": false,
-  "user_data": null,
-  "private_networking": null
+  "backups": "false",
+  "ipv6": "false",
+  "user_data": "null",
+  "private_networking": "null"
 }
 
-req = urllib2.Request("https://api.digitalocean.com/v2/droplets")
-req.add_header('Content-Type', 'application/json')
-req.add_header('Authorization', 'Bearer 6eb18afa72476ffa38a925eb0223d817c08a8db11d0fc9df371b78093b19fd0d')
+url = "https://api.digitalocean.com/v2/droplets"
+headers = {'Content-Type': 'application/json',
+            'Authorization': 'Bearer 6eb18afa72476ffa38a925eb0223d817c08a8db11d0fc9df371b78093b19fd0d'}
 
-response = urllib2.urlopen(req, json.dumps(new_droplet))
+r = requests.post(url, data=json.dumps(new_droplet), headers=headers)
+
+
+r = requests.get(url)
+
+
+#req = urllib2.Request("https://api.digitalocean.com/v2/droplets")
+#req.add_header('Content-Type', 'application/json')
+#req.add_header('Authorization', 'Bearer 6eb18afa72476ffa38a925eb0223d817c08a8db11d0fc9df371b78093b19fd0d')
+
