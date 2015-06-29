f709dabd6f157580f2158e34099aeadc22d6a969
Bert De Knijf <bert.deknijf@gmail.com>
2015-06-29 20:41:07 +0200

    create droplet test1

---
+++ droplet.py
@@ -0,0 +1,21 @@
+__author__ = 'bdknijf'
+import json
+import urllib2
+
+new_droplet = {
+  "name": "test_api",
+  "region": "ams2",
+  "size": "512mb",
+  "image": "ubuntu-14-04-x64",
+  "ssh_keys": "bert",
+  "backups": false,
+  "ipv6": false,
+  "user_data": null,
+  "private_networking": null
+}
+
+req = urllib2.Request("https://api.digitalocean.com/v2/droplets")
+req.add_header('Content-Type', 'application/json')
+req.add_header('Authorization', 'Bearer 6eb18afa72476ffa38a925eb0223d817c08a8db11d0fc9df371b78093b19fd0d')
+
+response = urllib2.urlopen(req, json.dumps(new_droplet))
